6640f1874d1f4f6e584522176ea27493a3df9485
root <anirudh-abhi>
2024-07-14 19:31:49 +0000

    Initial commit with microservices

---
+++ softwares.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+sudo apt update
+sudo apt install -y openjdk-17-jdk
+java -version
+sudo apt install maven
+mvn --version
+sudo git init
+git --version
+sudo apt install docker.io
+docker -version
+sudo apt install -y terraform
+terraform -v
